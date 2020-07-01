df83d9702a68dbbbd40b9bc33b4a30b9beee5e6a
Amonsta <amana.ammi@gmail.com>
2020-06-30 18:23:52 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+  end 
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+  end
+
 end
